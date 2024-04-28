07f0c6f91e7d37a1d3e7702202bbaca779d5c3e7
faiqamikom <faiq@students.amikom.ac.id>
2024-04-29 01:07:09 +0700

    admin-auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middelware' => 'admin.guest'], function () {
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+    });
+
+    Route::group(['middelware' => 'admin.auth'], function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+    });
 });
