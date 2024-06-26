8f8aeb761437dcd54f93aca22293f94b6a15cb29
Rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2024-06-26 21:39:42 +0700

    update terbaru

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Bootstrap demo</title>
+        <title>Technohub</title>
         <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-...="anonymous" />
@@ -12,13 +12,13 @@
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
             rel="stylesheet"
         />
-        <link rel="stylesheet" href="{{ asset('front-assets/style.css') }}" />
+      
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css"/>
         <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
         />
-
+        <link rel="stylesheet" href="{{ asset('front-assets/style.css') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta
             name="description"
